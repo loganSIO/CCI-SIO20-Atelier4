3a668c3b7659fec6d0560ecaa7ef80a333ce681b
logan <loganlenevez@gmail.com>
2022-03-18 14:26:43 +0100

    Ajout de tailwind + creation de la navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Button } from 'react-native-web'
+import { useNavigation } from '@react-navigation/native'
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button
+      title="Go to Chat Screen"
+      onPress={() => navigation.navigate("Chat")}
+      />
+    </View>
+  )
+}
+
+export default HomeScreen
